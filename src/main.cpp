7ce3d5ce4e302288f4eee55159812b86a0b0f1ec
Shavindu-Gayashan <shavindugaya@gmail.com>
2025-04-02 12:17:05 +0530

    Get veriable value from nextion

--- src/main.cpp
+++ src/main.cpp
@@ -3,15 +3,22 @@
 
 #define led 2
 
+bool ledState = false;  // Variable to track LED state
+
 //Buttons
 NexButton b0 = NexButton(0, 1, "b0");
+NexButton b1 = NexButton(0, 4, "b1");
 
-//Text
+//Texts
 NexText t0 = NexText(0, 2, "t0");
+
+//Variables
+NexVariable va0 = NexVariable(0, 3, "va0");
 
 
 NexTouch *nex_listen_list[] = {
   &b0,
+  &b1,
   NULL
 };
 
@@ -19,22 +26,40 @@ NexTouch *nex_listen_list[] = {
 // Function declarations
 void b0_pressed(void *ptr);
 void b0_relesed(void *ptr);
+void b1_pressed(void *ptr);
 
 // Function definitions
 void b0_pressed(void *ptr) {
-  digitalWrite(led, HIGH);  // Turn on LED
-  t0.setText("LED ON");  // Update text on the Nextion display
+  if (ledState) {
+    digitalWrite(led, LOW);
+    ledState = false;
+    t0.setText("LED OFF");
+  }else {
+    digitalWrite(led, HIGH);
+    ledState = true;
+    t0.setText("LED ON");
+  }
 }
 
 void b0_relesed(void *ptr) {
-  digitalWrite(led, LOW);  // Turn off LED
-  t0.setText("LED OFF");  // Update text on the Nextion display
+  //button relesed
 }
 
+void b1_pressed(void *ptr) {
+  Serial.println("Button pressed");
+  uint32_t value;
+  if (va0.getValue(&value)) {
+    Serial.print("Value is : ");
+    Serial.println(value);
+    t0.setText(String(value).c_str());  // Convert value to string
+  } else {
+    Serial.println("Failed to get value from va0");
+  }
+}
 
 void setup() {
   // Initialize Serial first
-  Serial.begin(115200);
+  Serial.begin(9600);
   delay(100);  // Allow time for Serial to initialize
   Serial.println("\nStarting");
   Serial.flush();  // Ensure all data is sent before proceeding
@@ -49,6 +74,7 @@ void setup() {
   // Attach button callbacks
   b0.attachPush(b0_pressed, &b0);
   b0.attachPop(b0_relesed, &b0);
+  b1.attachPush(b1_pressed, &b1);
   
 }
 
